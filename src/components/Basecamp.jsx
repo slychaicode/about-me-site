5d111750180d1065c0f5d6b083b653707d84e236
Kim Spaven <kim.spaven.business@gmail.com>
2025-04-23 07:27:40 -0400

    Updated apple-touch-icon and og:image

--- src/components/Basecamp.jsx
+++ src/components/Basecamp.jsx
@@ -22,7 +22,7 @@ export default function Basecamp({ unlockNext }) {
         <p className="text-slate-300 leading-relaxed text-lg">
           Hi! I’m Kim Spaven, currently an Advanced Technology Manager based in the U.S.
           I'm making an intentional leap - pivoting from defense tech to tech industry and relocating from Philadelphia to Vancouver.
-          When I saw Asana's Technical Engineering Manager role in Vancouver, it sparked this site - part portfolio, part personal introduction. 
+          When I saw Asana's Technical Lead Manager role in Vancouver, it sparked this site - part portfolio, part personal introduction. 
         </p>
 
         <ClimbButton
